0755a1e3d0116769192f9337fecca17ebe094ab0
Brayan Jose Perez Ramos <alu0101431185@ull.edu.es>
2024-11-11 19:17:33 +0000

    modelo user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,27 @@
 import { Request, Response } from 'express';
 import User from '../models/user';
 
+// Metodo para obtener todos los usuarios
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+// Metodo para obtener un usuario por su id
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+// Metodo para crear un usuario
 export const createUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const user = new User(req.body);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,22 @@
-const mongoose = require('mongoose');
+import { Document, Schema, model } from "mongoose";
 
-// export interface User {
-//   name: string;
-//   email: string;
-//   password: string;
-// }
+export interface UserDocumentInterface extends Document {
+  username: string;
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  creditCard: string;
+}
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema<UserDocumentInterface>({
+  username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
+  lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  creditCard: { type: String}
 });
 
-export const User = mongoose.model("User", userSchema);
+export const User = model("User", userSchema);
 export default User;
